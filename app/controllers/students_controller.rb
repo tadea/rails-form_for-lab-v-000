e59993ce4957f0fedcc33dffddfbc92f7d76e036
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-01 17:32:45 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -28,4 +28,10 @@ class StudentsController < ApplicationController
     @student.update(first_name: params[:first_name], last_name: params[:last_name])
     redirect_to student_path(@student)
   end
+
+  private
+  def student_params(*args)
+      params.require(:student).permit(*args)
+    end
+
 end
